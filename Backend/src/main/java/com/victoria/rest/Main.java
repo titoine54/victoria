5e51fff79299d665ffab825493578d4a91c04571
BenB-M <Benjamin.Bolduc-Meilleur@USherbrooke.ca>
2017-05-24 16:44:59 -0400

    initiale commit

---
+++ Backend/src/main/java/com/victoria/rest/Main.java
@@ -0,0 +1,25 @@
+package com.victoria.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by marom on 27/09/16.
+ */
+@Path("/")
+public class Main {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Victoria!";
+    }
+}
