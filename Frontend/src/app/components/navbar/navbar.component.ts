bb8ac895f1c7c3b9f2017d22edb6b086b6479cb9
Eric Matte <14026234@usherbrooke.ca>
2017-06-20 15:39:59 -0400

    Added side-navigation menu for smaller device screen.

--- Frontend/src/app/components/navbar/navbar.component.ts
+++ Frontend/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { GlobalVariablesService } from "app/services/global-variables.service";
 import { SettingsModalComponent } from "app/components/settings-modal/settings-modal.component";
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +9,7 @@ import { SettingsModalComponent } from "app/components/settings-modal/settings-m
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  isDevEnv: boolean = !environment.production;
   useBlackIcon: boolean = false;
 
   constructor(public global: GlobalVariablesService) { }
