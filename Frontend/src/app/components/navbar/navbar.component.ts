285816c1f2795e0566b784d1bc404230285a5516
Eric Matte <14026234@usherbrooke.ca>
2017-06-26 13:29:35 -0400

    Added a way to select the trimestre to show.

--- Frontend/src/app/components/navbar/navbar.component.ts
+++ Frontend/src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { GlobalVariablesService } from "app/services/global-variables.service";
 import { SettingsModalComponent } from "app/components/settings-modal/settings-modal.component";
 import { environment } from '../../../environments/environment';
+import { Dict } from "app/classes/dict.interface";
 
 @Component({
   selector: 'app-navbar',
@@ -11,6 +12,7 @@ import { environment } from '../../../environments/environment';
 export class NavbarComponent {
   isDevEnv: boolean = !environment.production;
   useBlackIcon: boolean = false;
+  selectOptions: string[] = ['Trimestre e17', 'Trimestre a16', 'Trimestre h17'];
 
   constructor(public global: GlobalVariablesService) { }
 
@@ -21,4 +23,10 @@ export class NavbarComponent {
     this.useBlackIcon = (header.offsetHeight > 100);
   }
 
+  /** Select a new trimestre to show
+   * @param {Dict<string>} trimestre The trimestre to fetch data from
+   */
+  selectTrimestre(trimestre: Dict<string>) {
+    console.log(trimestre);
+  }
 }
