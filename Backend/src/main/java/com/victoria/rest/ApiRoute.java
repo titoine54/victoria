ee6042dc17b0800c0763f004575a3e4b66421976
BenB-M <Benjamin.Bolduc-Meilleur@USherbrooke.ca>
2017-06-28 19:34:10 -0400

    Api route corrections

--- Backend/src/main/java/com/victoria/rest/ApiRoute.java
+++ Backend/src/main/java/com/victoria/rest/ApiRoute.java
@@ -17,7 +17,6 @@ import javax.ws.rs.core.MediaType;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Iterator;
 
 @Path("/")
