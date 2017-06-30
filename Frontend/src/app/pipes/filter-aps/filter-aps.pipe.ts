88cf0a90fd93e652e1155ef63ad723c2ee83af4c
mate2002 <14026234@usherbrooke.ca>
2017-06-29 20:11:17 -0400

    i Search anything !

--- Frontend/src/app/pipes/filter-aps/filter-aps.pipe.ts
+++ Frontend/src/app/pipes/filter-aps/filter-aps.pipe.ts
@@ -10,20 +10,31 @@ export class FilterApsPipe implements PipeTransform {
   /** Filter the list of Aps by searching for matching value with Aps and Competences
    * @param {Ap[]} Aps The list of "Activité Pédagogique" to filter
    * @param {string} filter The string value to use as a filter
+   * @param {Evaluation[]} evaluations The list of all evaluations
    * @return {Ap[]}} The filtered list
    */
-  transform(aps: Ap[], filter: string): Ap[] {
+  transform(aps: Ap[], filter: string, evaluations?: Evaluation[]): Ap[] {
     if (filter == undefined || filter == '') {
       return aps;
     } else {
       filter = filter.toLocaleLowerCase();
       return aps.filter(ap => {
+        // Search in AP code and title
         if (ap.apCode.toLowerCase().includes(filter)) return true;
         if (ap.titre.toLocaleLowerCase().includes(filter)) return true;
+        // Search in competences description
         if (ap.competences) {
           for (let competence of ap.competences) {
             if (competence.description) {
               if (competence.description.toLocaleLowerCase().includes(filter)) return true;
+            }
+          }
+        }
+        // Now search in evaluations
+        if (evaluations) {
+          for (let evaluation of evaluations) {
+            if (evaluation.associatedAps[ap.apCode] != undefined) {
+              if (evaluation.titre.toLocaleLowerCase().includes(filter)) return true;
             }
           }
         }
