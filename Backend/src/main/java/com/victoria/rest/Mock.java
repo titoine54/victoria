d91d211e6a4fa37d31554eea8ba1086634809deb
mate2002 <14026234@usherbrooke.ca>
2017-07-12 14:57:43 -0400

    Added missing AP in mock.

--- Backend/src/main/java/com/victoria/rest/Mock.java
+++ Backend/src/main/java/com/victoria/rest/Mock.java
@@ -51,24 +51,54 @@ public class Mock {
         return "{\n" +
                 "  \"aps\": [\n" +
                 "    {\n" +
-                "      \"apCode\": \"GIF500\",\n" +
-                "      \"titre\": \"Chasser\",\n" +
-                "      \"credit\": 2,\n" +
-                "      \"description\": \"Devenir un maître chasseur\",\n" +
-                "      \"competences\": [\n" +
-                "          {\"competenceNumero\": 1, \"description\": \"Bien viser\"},\n" +
-                "          {\"competenceNumero\": 2, \"description\": \"Bien se cacher\"}\n" +
-                "      ]\n" +
+                "        \"apCode\": \"GIF501\", \"titre\": \"Droit\", \"credit\": 2, \"description\": \"Devenir un maître juridique et dominer le monde\",\n" +
+                "        \"competences\": []\n" +
                 "    },\n" +
                 "    {\n" +
-                "      \"apCode\": \"GIF510\",\n" +
-                "      \"titre\": \"Cuisiner\",\n" +
-                "      \"credit\": 3,\n" +
-                "      \"description\": \"Devenir un maître cuisinier\",\n" +
-                "      \"competences\": [\n" +
-                "          {\"competenceNumero\": 1, \"description\": \"Bien préparer\"},\n" +
-                "          {\"competenceNumero\": 2, \"description\": \"Bien cuire\"}\n" +
-                "    ]\n" +
+                "        \"apCode\": \"GIF500\", \"titre\": \"Conception d\\'un système embarqué et réseauté\", \"credit\": 3, \"description\": null,\n" +
+                "        \"competences\": [\n" +
+                "            { \"competenceNumero\": 1, \"description\": \"Faire un réseau\" },\n" +
+                "            { \"competenceNumero\": 2, \"description\": \"Embarquer sur un système\" }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"apCode\": \"GIF510\", \"titre\": \"Systèmes à microprocesseurs\", \"credit\": 2, \"description\": \"Exploiter une carte comportant un microcontrôleur et des circuits d\\'entrée/sortie en utilisant la documentation technique des composants et des logiciels qui la constituent; développer et mettre au point un logiciel sur un système embarqué en utilisant des outils de développement croisés.\",\n" +
+                "        \"competences\": [\n" +
+                "            { \"competenceNumero\": 1, \"description\": \"Exploiter une carte comportant un microcontrôleur et des circuits d\\'entrée-sortie en utilisant la documentation technique des composants et des logiciels qui la constituent.\" },\n" +
+                "            { \"competenceNumero\": 2, \"description\": \"Développer et mettre au point un logiciel sur un système embarqué en utilisant des outils de développement croisés.\" }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"apCode\": \"GIF520\", \"titre\": \"Propriétés des matériaux\", \"credit\": 2, \"description\": null,\n" +
+                "        \"competences\": [\n" +
+                "            { \"competenceNumero\": 1, \"description\": \"Savoir si un matériel est mou\" },\n" +
+                "            { \"competenceNumero\": 2, \"description\": \"Appliquer la règle du pouce\" },\n" +
+                "            { \"competenceNumero\": 2, \"description\": \"Calculer des calculs de manière scientifique\" }\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"apCode\": \"GIF530\", \"titre\": \"Circuits d'entrées/sorties et d'interfaces\", \"credit\": 1, \"description\": null,\n" +
+                "        \"competences\": [\n" +
+                "            { \"competenceNumero\": 1, \"description\": \"Circuits d'entrées\" },\n" +
+                "            { \"competenceNumero\": 2, \"description\": \"Circuits de sorties\" },\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"apCode\": \"GIF540\", \"titre\": \"Noyaux temps réel et programmation concurrente\", \"credit\": 1, \"description\": null,\n" +
+                "        \"competences\": [\n" +
+                "            { \"competenceNumero\": 1, \"description\": \"Faire de la propagande en temps réel dans un système donné\" },\n" +
+                "        ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"apCode\": \"GIF550\", \"titre\": \"Systèmes embarqués réseautés\", \"credit\": 1, \"description\": null,\n" +
+                "        \"competences\": []\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"apCode\": \"GIF560\", \"titre\": \"Statistiques et fiabilité des systèmes\", \"credit\": 2, \"description\": null,\n" +
+                "        \"competences\": [\n" +
+                "            { \"competenceNumero\": 1, \"description\": \"Loi normal\" },\n" +
+                "            { \"competenceNumero\": 2, \"description\": \"Rendre un système fiable\" }\n" +
+                "        ]\n" +
                 "    }\n" +
                 "  ],\n" +
                 "  \"evaluations\": [\n" +
