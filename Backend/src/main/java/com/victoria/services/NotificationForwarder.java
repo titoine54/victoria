939bd89345b33d861f772ad700ba84719b6cdccc
BenB-M <Benjamin.Bolduc-Meilleur@USherbrooke.ca>
2017-07-19 20:30:10 -0400

    Send Test notifications to other team, need to add our values from the db

--- Backend/src/main/java/com/victoria/services/NotificationForwarder.java
+++ Backend/src/main/java/com/victoria/services/NotificationForwarder.java
@@ -16,19 +16,20 @@ public class NotificationForwarder implements Runnable{
 
         //{ "cip": "gosa3202", "title": "Titre test", "description": "Desc Test", "url": "", "idChannel": 1, "idCategory": 8, "dateEmitted": "2017-07-18", "dateExpired": "2017-07-19" }
         JSONObject o = new JSONObject();
-        o.put("cip", "bolb2201");
-        o.put("title", "test title");
-        o.put("description", "test desc");
-        o.put("url", "");
+        o.put("cip", "marf3402");
+        o.put("title", "test title"+Double.toString(Math.random()));
+        o.put("description", "test desc4");
+        o.put("url", "www.google.com");
         o.put("idChannel", "1");
         o.put("idCategory", "8");
         o.put("dateEmitted", "2017-07-19");
         o.put("dateExpired", "2017-07-20");
 
         try {
-            URL url = new URL("https://s6ie1718.gel.usherbrooke.ca/api/developpeur/post_notification");
+            URL url = new URL("http://s6ie1718.gel.usherbrooke.ca/api/developpeur/post_notification");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setDoOutput(true);
+            conn.setDoInput(true);
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "application/json");
 
@@ -37,6 +38,10 @@ public class NotificationForwarder implements Runnable{
             OutputStream os = conn.getOutputStream();
             os.write(input.getBytes());
             os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                System.out.println("Failed : HTTP error code : " + conn.getResponseCode());
+            }
 
             conn.disconnect();
         }
