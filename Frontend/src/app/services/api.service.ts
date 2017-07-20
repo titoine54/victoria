2d8dc20dd13a944216f37c1af1d1aeabb307c7ea
mate2002 <14026234@usherbrooke.ca>
2017-07-19 23:45:08 -0400

    Link to notification api.

--- Frontend/src/app/services/api.service.ts
+++ Frontend/src/app/services/api.service.ts
@@ -86,7 +86,7 @@ export class ApiService {
    * @return {Observable} The observable for the caller
    */
   public markNotificationAsRead(notificationId: number): Observable<any> {
-    return this.http.get(`${environment.apiUrl}/notify/${notificationId}`, this.getRequestOptions())
+    return this.http.get(`${environment.apiUrl}/notification/${notificationId}`, this.getRequestOptions())
       .map((res: Response) => res.json())
       .catch(this.handleError);
   }
