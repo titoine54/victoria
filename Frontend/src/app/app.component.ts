01598d38acdcf241454e2814323489bb616c575c
Félix Martel-Denis <lixtelnis@gmail.com>
2017-07-25 09:12:32 -0400

    Unwrap http call and processing using promise

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -20,64 +20,75 @@ export class AppComponent {
   currentYear: number = new Date().getFullYear();
 
   constructor(private apiService: ApiService, public global: GlobalVariablesService) {
-    apiService.getUserData().subscribe(
-      (data: any) => {
-        global.user = new User(data.cip, data.firstName, data.lastName, data.email);
-        this.loadUserNotes();
-      },
-      err => Materialize.toast('Une erreur s\'est produite lors du téléchargement des donnees de l\'utilisateur.', 4000));
+    try {
+      this.loadUserInfo();
+      this.loadUserNotes();
+      this.loadNotesStats();
+    }
+    catch(err) {
+      this.global.apList = null;
+      Materialize.toast(err.message, 4000);
+    }
   }
 
-  loadUserNotes() {
-    this.apiService.getNotes().subscribe(
-      (data: any) => {
-        this.global.apList = [];
-        this.global.evaluations = [];
-        this.global.nouvelles = [];
+  async loadUserInfo() {
+    try {
+      let data = await this.apiService.getUserData().toPromise();
+      this.global.user = new User(data.cip, data.firstName, data.lastName, data.email);
+    }
+    catch(err) {
+      throw new Error('Une erreur s\'est produite lors du téléchargement des donnees de l\'utilisateur.');
+    }
+  }
 
-        for (let ap of data.aps) {
-          this.global.apList.push(new Ap(ap.apCode, ap.titre, ap.competences, ap.description, ap.credit));
-        }
+  async loadUserNotes() {
+    try {
+      let data = await this.apiService.getNotes().toPromise();
 
-        for (let e of data.evaluations) {
-          this.global.evaluations.push(new Evaluation(e.nom, e.activites, e.evaluationId, e.estNouveau, e.individuel));
-        }
+      this.global.apList = [];
+      this.global.evaluations = [];
+      this.global.nouvelles = [];
 
-        if (data.notifications) {
-          for (let n of data.notifications) {
-            this.global.nouvelles.push(new Nouvelle(n.notificationID, n.evaluationID, n.evaluationNom, n.descriptionNotification));
-            var associatedEvaluation = this.global.evaluations.find(e => e.evaluationId == n.evaluationID);
-            if (associatedEvaluation) {
-              associatedEvaluation.estNouveau = true;
-            }
+      for (let ap of data.aps) {
+        this.global.apList.push(new Ap(ap.apCode, ap.titre, ap.competences, ap.description, ap.credit));
+      }
+
+      for (let e of data.evaluations) {
+        this.global.evaluations.push(new Evaluation(e.nom, e.activites, e.evaluationId, e.estNouveau, e.individuel));
+      }
+
+      if (data.notifications) {
+        for (let n of data.notifications) {
+          this.global.nouvelles.push(new Nouvelle(n.notificationID, n.evaluationID, n.evaluationNom, n.descriptionNotification));
+          var associatedEvaluation = this.global.evaluations.find(e => e.evaluationId == n.evaluationID);
+          if (associatedEvaluation) {
+            associatedEvaluation.estNouveau = true;
           }
         }
-
-        this.loadNotesStats();
-      },
-      err => {
-        this.global.apList = [];
-        Materialize.toast('Une erreur s\'est produite lors du téléchargement des notes.', 4000);
-      });
+      }
+    }
+    catch(err) {
+      throw new Error('Une erreur s\'est produite lors du téléchargement des notes.');
+    }
   }
 
-  loadNotesStats() {
-    this.apiService.getStats().subscribe(
-      (data: any) => {
-        if (data.statistiques != undefined) {
-          var stats: Statistiques[] = data.statistiques;
-          for (let stat of stats) {
-            loop:
-            for (let evaluation of this.global.evaluations) {
-              if (evaluation.evaluationId == stat.evaluationId) {
-                for (var apCode in evaluation.associatedAps) {
-                  if (apCode == stat.apCode) {
-                    for (let note of evaluation.associatedAps[apCode]) {
-                      if (note.competenceNumero == stat.competenceNumero) {
-                        note.moyenne = stat.moyenne;
-                        note.ecartType = stat.ecartType;
-                        break loop;
-                      }
+  async loadNotesStats() {
+    try {
+      let data = await this.apiService.getStats().toPromise();
+
+      if (data.statistiques != undefined) {
+        var stats: Statistiques[] = data.statistiques;
+        for (let stat of stats) {
+          loop:
+          for (let evaluation of this.global.evaluations) {
+            if (evaluation.evaluationId == stat.evaluationId) {
+              for (var apCode in evaluation.associatedAps) {
+                if (apCode == stat.apCode) {
+                  for (let note of evaluation.associatedAps[apCode]) {
+                    if (note.competenceNumero == stat.competenceNumero) {
+                      note.moyenne = stat.moyenne;
+                      note.ecartType = stat.ecartType;
+                      break loop;
                     }
                   }
                 }
@@ -85,11 +96,11 @@ export class AppComponent {
             }
           }
         }
-        this.global.evaluations = [...this.global.evaluations];
-      },
-      err => {
-        this.global.apList = [];
-        Materialize.toast('Une erreur s\'est produite lors du téléchargement des notes.', 4000);
-      });
+      }
+      this.global.evaluations = [...this.global.evaluations];
+    }
+    catch(err) {
+      throw new Error('Une erreur s\'est produite lors du téléchargement des notes.');
+    }
   }
 }
